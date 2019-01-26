8e5d5c778310ab3c7a0b0b4e8ccb6fdd40c9dae1
xinminhang <1137197242@qq.com>
2019-01-26 12:03:50 +0800

    人物移动/地图旋转

---
+++ minigame/Assets/Scripts/Map/MapRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRotate : MonoBehaviour
+{
+    public float mapRotateV=40f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0f,0f,Time.deltaTime*mapRotateV,Space.Self);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0f, 0f, Time.deltaTime * -mapRotateV, Space.Self);
+        }
+    }
+}
