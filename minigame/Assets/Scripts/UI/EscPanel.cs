ca0d89d96c5f399f8a503205f1a2d50fa0b94223
gaotaobo <1121252070@qq.com>
2019-01-26 11:58:12 +0800

    添加游戏暂停、继续、退出

---
+++ minigame/Assets/Scripts/UI/EscPanel.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 管理EscPanel面板
+/// </summary>
+public class EscPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject escPanel;
+
+
+
+
+    private void HideUiObj()
+    {
+        escPanel.SetActive(false);
+
+    }
+
+
+    /// <summary>
+    /// 继续游戏
+    /// </summary>
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        HideUiObj();
+
+
+
+    }
+
+     /// <summary>
+     /// 退出游戏
+     /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
+    /// <summary>
+    /// 游戏帮助
+    /// </summary>
+    public void GameHelp()
+    {
+
+    }
+
+    /// <summary>
+    /// 重新开始
+    /// </summary>
+    public void RestartGame()
+    {
+        //SceneManager.LoadScene();
+        HideUiObj();
+
+    }
+
+
+
+
+
+
+
+
+}
